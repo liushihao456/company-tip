5ac050384cc20f4abd6feba9ae26e3079939a1ec
lshnmoptim <shihao.liu@nmoptim.com.cn>
2022-03-27 17:19:13 +0800

    Added treemacs-nerd theme for TUI

--- company-tip-autoloads.el
+++ company-tip-autoloads.el
@@ -1,4 +1,4 @@
-;;; company-tip-autoloads.el --- automatically extracted autoloads
+;;; company-tip-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
 ;;
 ;;; Code:
 
@@ -12,10 +12,20 @@
 (autoload 'company-tip-local-mode "company-tip" "\
 Provides documentation popups for `company-mode' using `popup-tip'.
 
-If called interactively, enable Company-Tip-Local mode if ARG is
-positive, and disable it if ARG is zero or negative.  If called
-from Lisp, also enable the mode if ARG is omitted or nil, and
-toggle it if ARG is `toggle'; disable the mode otherwise.
+This is a minor mode.  If called interactively, toggle the
+`Company-Tip-Local mode' mode.  If the prefix argument is
+positive, enable the mode, and if it is zero or negative, disable
+the mode.
+
+If called from Lisp, toggle the mode if ARG is `toggle'.  Enable
+the mode if ARG is nil, omitted, or is a positive number.
+Disable the mode if ARG is a negative number.
+
+To check whether the minor mode is enabled in the current buffer,
+evaluate `company-tip-local-mode'.
+
+The mode's hook is called both when the mode is enabled and when
+it is disabled.
 
 \(fn &optional ARG)" t nil)
 
@@ -33,17 +43,21 @@ or call the function `company-tip-mode'.")
 
 (autoload 'company-tip-mode "company-tip" "\
 Toggle Company-Tip-Local mode in all buffers.
-With prefix ARG, enable Company-Tip mode if ARG is positive;
-otherwise, disable it.  If called from Lisp, enable the mode if
-ARG is omitted or nil.
+With prefix ARG, enable Company-Tip mode if ARG is positive; otherwise, disable
+it.
 
-Company-Tip-Local mode is enabled in all buffers where
-`company-tip-local-mode' would do it.
+If called from Lisp, toggle the mode if ARG is `toggle'.
+Enable the mode if ARG is nil, omitted, or is a positive number.
+Disable the mode if ARG is a negative number.
+
+Company-Tip-Local mode is enabled in all buffers where `company-tip-local-mode'
+would do it.
+
 See `company-tip-local-mode' for more information on Company-Tip-Local mode.
 
 \(fn &optional ARG)" t nil)
 
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "company-tip" '("company-tip-")))
+(register-definition-prefixes "company-tip" '("company-tip-"))
 
 ;;;***
 
@@ -51,6 +65,6 @@ See `company-tip-local-mode' for more information on Company-Tip-Local mode.
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
+;; coding: utf-8-emacs-unix
 ;; End:
 ;;; company-tip-autoloads.el ends here
